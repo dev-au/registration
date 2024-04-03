26d5c7231f3faa0c615f29aaf15c33810e72759e
Abdulloh Umar <abdulloh.umar2006@gmail.com>
2024-04-03 17:11:26 +0500

    Change some information

--- resources/login.py
+++ resources/login.py
@@ -13,7 +13,7 @@ async def login_for_access_token(form_data: LoginModel):
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"user": user.username}, expires_delta=access_token_expires
+        data={"sub": user.username}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
 
