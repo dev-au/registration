a0f70ce672a1e0ab6f32e4db96147f768b0b37cd
Abdulloh Umar <abdulloh.umar2006@gmail.com>
2024-04-06 14:27:30 +0500

    Updated new main.py

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ register_tortoise(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
